cb0488ce83130827448ad409c21ad763df9cc201
plasticneuron <plasticneuron@posteo.net>
2019-10-27 20:27:17 +0000

    Assign various useful elements to variables for quick access, and made a first stab at function for input to the display

--- script.js
+++ script.js
@@ -22,19 +22,84 @@
 - can catch all button press events on the input-container div because they will all bubble up through it
 */
 
+// assign event-catching container to a variable
 let pad = document.querySelector('#input-container');
-let nums = [...document.querySelectorAll('.button-input')];
-let ops = [...document.querySelectorAll('.button-operator')];
-console.log(nums);
-console.log(ops);
 
-// input
+// assign display span, which will accept inputs to eventually give the expression to execute, to a variable
+let display = document.querySelector('#display');
+
+// assign arrays of button categories to variables
+let inputs = [...document.querySelectorAll('.button-input')];
+let nonZeroNums = inputs.slice(0,9);
+let operators = [...document.querySelectorAll('.button-operator')];
+
+// assign special buttons to variables
+let zeroButton = document.querySelector('#zero');
+let clearButton = document.querySelector('#clear');
+let pointButton = document.querySelector('#point');
+let equalsButton = document.querySelector('#execute');
+let memButton = document.querySelector('#memory');
+let ansButton = document.querySelector('#last-answer');
+let backButton = document.querySelector('#delete');
+let addButton = document.querySelector('#add');
+let subtractButton = document.querySelector('#subtract');
+let multiplyButton = document.querySelector('#multiply');
+let divideButton = document.querySelector('#divide');
+
+// assign useful regular expressions
+let opRegex = /[+-/*]+/ // matches any run of operators (possibilities: +,-,*,/,+-,*-,/-)
+let numRegex = /[0-9]+/ // matches any run of numbers
+
+// make function to handle inputs to display/expression
 var addToDisplay = function(event) {
-  let display = document.getElementById('display'); // now display points to the display element
-  //let displayString = display.innerHTML; // this just copies the text in the display to a primitive variable displayString
-  //console.log(displayString);
-  //display.innerHTML = '1000 +';
-  
+  // initialise diagnostic strings
+  let lastChar = display.textContent.slice(-1);
+  let lastNum = (display.textContent.match(/[.0-9]+$/) || [''])[0]; // captures last run of digits (w/ or w/o decimal point), or else resolves to an empty string
+  let lastOps = (display.textContent.match(/[+-/*]+$/) || [''])[0]; // captures last run of operators, or else resolves to an empty string
+
+  // first deal with initial/reset case (display text == '0')
+  if (display.textContent.length == 1 && lastChar == '0') {
+    if (nonZeroNums.includes(event.target)) {
+      display.textContent = event.target.textContent;
+    }
+    else if (event.target == pointButton) {
+      display.textContent = '0.';
+    }
+  }
+
+  // then deal with normal flow
+  else {
+    // input: 0
+    // only case where zero can't be added is if this produces multiple zeroes at start of a number
+    if (event.target == zeroButton) {
+      if (!(lastNum.length == 1 && lastChar == '0')) {
+        display.textContent += '0';
+      }
+    }
+    // input: [1-9]
+    // can be added at any point
+    if (nonZeroNums.includes(event.target)) {
+      display.textContent += event.target.textContent;
+    }
+    // input: .
+    // can be added after any character, but not twice in one number
+    if (!(lastNum.includes('.'))) {
+      if (opRegex.test(lastChar)) {
+        display.textContent += '0.';
+      }
+      else {
+        display.textContent += '.';
+      }
+    }
+    // input: [+-/*]
+    // can only be added to a number, except -, which can be added once to any operator other than itself
+    if (lastChar != '-' && lastOps.length < 2) { // i.e. last run is either a number or a single operator other than -
+      event.target == divideButton ? display.textContent += '/' :
+        event.target == multiplyButton ? display.textContent =+ '*' :
+        display.textContent += event.target.textContent;
+    }
+  }
+  // not yet handled: case when result is displayed
 }
 
-pad.addEventListener('click', addToDisplay);
+pad.addEventListener('click',addToDisplay);

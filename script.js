18ae32ecc9d23a2bfab35deb85f1638c0aaf6ce0
plasticneuron <plasticneuron@posteo.net>
2019-10-28 00:18:47 +0000

    Corrected semi-colons in object declaration to commas

--- script.js
+++ script.js
@@ -54,20 +54,20 @@ var justExecuted = false;
 
 // make dictionary object to relate textContent of elements to their ids
 let textContents = {
-  0: 'zero';
-  1: 'one';
-  2: 'two';
-  3: 'three';
-  4: 'four';
-  5: 'five';
-  6: 'six';
-  7: 'seven';
-  8: 'eight';
-  9: 'nine';
-  .: 'point';
-  +: 'add';
-  -: 'subtract';
-  &divide: 'divide';
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  .: 'point',
+  +: 'add',
+  -: 'subtract',
+  &divide: 'divide',
   &multiply: 'multiply';
 }
 
